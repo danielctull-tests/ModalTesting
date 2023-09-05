a9ce347ff9815e2f8ff9a20078222fccef3b7e2d
Daniel Tull <dt@danieltull.co.uk>
2023-09-05 16:59:28 +0100

    Initial Commit

---
+++ ModalTesting/ModalTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalTestingApp.swift
+//  ModalTesting
+//
+//  Created by Tull, Daniel on 05/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ModalTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
